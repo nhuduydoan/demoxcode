92842a1bf7a48a105d56553af71996d6ab8d79e4
nhuduydoan <nhuduydoan@CPU12016s-Mac-mini.local>
2017-12-13 09:25:49 +0700

    ipdate something

--- DemoXcodeTests/DemoXcodeTests.m
+++ DemoXcodeTests/DemoXcodeTests.m
@@ -54,38 +54,55 @@
 }
 
 - (void)testMultiDownloadOneURLWithMultiThread {
-    XCTestExpectation *expect = [self expectationWithDescription:@"expect"];
+//    XCTestExpectation *expect = [self expectationWithDescription:@"expect"];
     NSURL *imageURL = [NSURL URLWithString:_imageLink];
     NSURL *fileURL = [NSURL fileURLWithPath:[[sFileManager rootFolderPath] stringByAppendingPathComponent:@"Cun con.PNG"]];
     dispatch_queue_t concurrentQueue = dispatch_queue_create("concurrentqueue", DISPATCH_QUEUE_CONCURRENT);
     NSMutableArray *arr = [NSMutableArray new];
     
+    NSLock *lock = [NSLock new];
+    [lock lock];
+    [lock tryLock];
     dispatch_group_t group = dispatch_group_create();
     
-    for (int i = 0; i < 100; i ++) {
-        //                    dispatch_group_enter(group);
-        dispatch_async(concurrentQueue, ^{
+    dispatch_block_t block = dispatch_block_create(DISPATCH_BLOCK_BARRIER, ^{
+        NSLog(@"Before Sleep 1");
+//        sleep(2);
+        for (int i = 0; i < 100; i ++) {
+            //            dispatch_group_enter(group);
+            
             DXDownloadComponent *component = [sDownloadManager downloadURL:imageURL toFilePath:fileURL completionHandler:nil error:nil];
             if (component) {
                 NSLog(@"add:%d", i);
-                [arr addObject:component];
+//                [arr addObject:component];
             }
             NSLog(@"%d", i);
-            //                        dispatch_group_leave(group);
-        });
-    }
-    dispatch_barrier_async(concurrentQueue, ^{
-        
-        NSLog(@"===");
-        XCTAssertEqual(arr.count, 1, @"Components needs is once");
-        [expect fulfill];
+            //            dispatch_group_leave(group);
+        }
+        NSLog(@"After Sleep 2");
+        NSLog(@"Loi gi a=======");
     });
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), block);
+//    dispatch_barrier_async(concurrentQueue, ^{
+//
+//        NSLog(@"===");
+//        XCTAssertEqual(arr.count, 1, @"Components needs is once");
+//        [expect fulfill];
+//    });
     //    dispatch_group_notify(group, dispatch_get_main_queue(), ^{
     //        XCTAssertEqual(arr.count, 1, @"Components needs is once");
-    //        [expect fulfill];
+//            [expect fulfill];
     //    });
+    NSLog(@"Xong viec roi ne");
+    dispatch_block_wait(block, DISPATCH_TIME_FOREVER);
+    NSLog(@"A hu hu");
+//    dispatch_group_wait(group, time);
+//    NSLog(@"A hi hi");
+//    [expect fulfill];
+    XCTAssertEqual(arr.count, 0, @"Components needs is once");
     
-    [self waitForExpectationsWithTimeout:10 handler:nil];
+//    [self waitForExpectationsWithTimeout:10 handler:nil];
 }
 
 @end
